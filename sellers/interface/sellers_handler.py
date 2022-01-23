add49964d6f46d4106672249a4768540da72b6ca
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-23 14:56:48 -0600

    BREAKING CHANGE(sellers): update function name for register seller on handler

--- sellers/interface/sellers_handler.py
+++ sellers/interface/sellers_handler.py
@@ -1,6 +1,6 @@
 import json
 from sellers.application.register_seller import registerSeller
 
-def register_seller_handler(event, context):
+def registerSellerHandler(event, context):
   body = json.loads(event['body'])
   return registerSeller(name=body['name'], email=body['email'], password=body['password'])
