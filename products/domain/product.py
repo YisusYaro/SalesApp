20b05cefe7a0994a322280ddc02c18412cd0bc39
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-24 17:30:27 -0600

    feat: add product domain element

---
+++ products/domain/product.py
@@ -0,0 +1,4 @@
+class Product:
+
+  def __init__(self, _dict):
+    self.__dict__.update(_dict)
---
+++ products/domain/product_factory.py
@@ -0,0 +1,8 @@
+from unicodedata import category
+from products.domain.product import Product
+
+def create(id, name, price, category):
+  return Product({'id':id, 'name':name, 'price':price, 'category':category})
+
+def reconstitute(id, name, price, category):
+  return Product({'id':id, 'name':name, 'price':price, 'category':category})
