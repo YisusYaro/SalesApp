d318f3b191723503404a4345caff1e2434171a89
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-24 18:51:26 -0600

    feat: add find seller query

---
+++ sellers/infraestructure/queries/seller_query.py
@@ -0,0 +1,13 @@
+from sellers.infraestructure.models.seller_model import SellerModel
+from sellers.domain.sellers_factory import reconstitute
+
+def find():
+  models = SellerModel.query(hash_key='sellers');
+  return modelsToSellers(models)
+
+def modelsToSellers(models):
+  sellers = []
+  for model in models:
+    SK = model.SK.split('#')
+    sellers.append(reconstitute(id=SK[1], name=model.name, email=SK[0], password=model.password))
+  return sellers
