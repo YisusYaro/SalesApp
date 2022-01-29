681a62b94ab20c219923d177bc23148a73c37a38
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-29 12:21:49 -0600

    BREAKING CHANGE(client): remove set password method

--- clients/domain/client.py
+++ clients/domain/client.py
@@ -1,4 +1,3 @@
-import bcrypt
 from clients.domain.error import Error
 
 class Client(object):
@@ -6,7 +5,3 @@ class Client(object):
   def __init__(self, _dict):
     self.__dict__.update(_dict)
   
-  def setPassword(self, password):
-    if(password == ''):
-      raise Exception(Error.CAN_NOT_SET_PASSWORD)
-    self.password = bcrypt.hashpw(password, bcrypt.gensalt())
