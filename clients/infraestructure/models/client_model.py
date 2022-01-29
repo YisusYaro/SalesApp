85de291b6c1d2a1d7968379d7d5a1b2ece7493a8
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-29 12:23:13 -0600

    BREAKING CHANGE(clients): remove password field on model

--- clients/infraestructure/models/client_model.py
+++ clients/infraestructure/models/client_model.py
@@ -1,12 +1,12 @@
+import os
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 
 
 class ClientModel(Model):
     class Meta:
-        table_name = 'App'
-        region = 'us-east-1'
+        table_name = os.environ.get('TABLE')
+        region = os.environ.get('REGION')
     PK = UnicodeAttribute(hash_key=True)
     SK = UnicodeAttribute(range_key=True)
     name = UnicodeAttribute()
-    password = UnicodeAttribute()
