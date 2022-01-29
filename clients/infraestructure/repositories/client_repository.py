32bffd6d421aabc24e9fd0bba6f03b4e5795ba8e
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-29 12:24:53 -0600

    refactor: use dependencies injections instead functions on client repository

--- clients/infraestructure/repositories/client_repository.py
+++ clients/infraestructure/repositories/client_repository.py
@@ -1,28 +1,35 @@
 from ulid import ULID
+from shared.infraestructure.data_structures.singleton import Singleton
 from clients.infraestructure.models.client_model import ClientModel
-from clients.domain.client_factory import reconstitute
+from clients.domain.client_factory import ClientFactory
 
-def getId():
-  return str(ULID())
 
-def save(client):
-  PK = 'clients'
-  SK = '{email}#{id}'.format(email=client.email, id=client.id)
-  model = ClientModel(PK=PK, SK=SK, name=client.name, password=client.password)
-  model.save() 
-  return
+class ClientRepository(metaclass=Singleton):
 
-def findByEmail(email):
-  for model in ClientModel.query(
-    hash_key='clients',
-    range_key_condition=ClientModel.SK.startswith(email),
-    limit=1
-  ):
-    return modelToClient(model)
-  return False  
+  def __init__(self):
+    self.clientFactory = ClientFactory()
 
-def modelToClient(model):
-  SK = model.SK.split('#')
-  return reconstitute(id=SK[0], name=model.name, email=SK[1], password=model.password)
-  
+  def getId(self):
+    return str(ULID())
+
+  def save(self, client):
+    PK = 'clients'
+    SK = '{email}#{id}'.format(email=client.email, id=client.id)
+    model = ClientModel(PK=PK, SK=SK, name=client.name)
+    model.save()
+    return
+
+  def findByEmail(self, email):
+    for model in ClientModel.query(
+      hash_key='clients',
+      range_key_condition=ClientModel.SK.startswith(email),
+      limit=1
+    ):
+      return self.__modelToClient(model)
+    return False  
+
+  def __modelToClient(self, model):
+    SK = model.SK.split('#')
+    return self.clientFactory.reconstitute(id=SK[0], name=model.name, email=SK[1])
+    
 
