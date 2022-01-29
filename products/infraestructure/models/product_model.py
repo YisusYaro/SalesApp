b77b8e067f47b0db66bf5c82e1cbadadfb8d0cb7
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-29 12:27:42 -0600

    feat: use environment instead hardcoded variables

--- products/infraestructure/models/product_model.py
+++ products/infraestructure/models/product_model.py
@@ -1,12 +1,12 @@
-from unicodedata import category
+import os
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute
 
 
 class ProductModel(Model):
     class Meta:
-        table_name = 'App'
-        region = 'us-east-1'
+        table_name = os.environ.get('TABLE')
+        region = os.environ.get('REGION')
     PK = UnicodeAttribute(hash_key=True)
     SK = UnicodeAttribute(range_key=True)
     name = UnicodeAttribute()
