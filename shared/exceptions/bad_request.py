8ff7b07ad1d05cf6bf998be410b367b6cba25d8e
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-01-30 23:47:57 -0600

    feat: add bad request exception

---
+++ shared/exceptions/bad_request.py
@@ -0,0 +1,11 @@
+"""Raises an bad request exception ."""
+
+
+class BadRequestException(Exception):
+    """Class of the BadRequestException class .
+
+    Args:
+        Exception ([type]): [description]
+    """
+
+    pass
